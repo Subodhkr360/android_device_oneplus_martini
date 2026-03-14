29e161e22bc8090085cf610f3800bd871789241f
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-03-14 15:04:25 +0530

    Viperfx

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+#for viperfx
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/martini/BoardConfigVendor.mk
