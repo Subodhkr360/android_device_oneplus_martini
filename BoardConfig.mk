60a0e724194e21881c195adad81d7bc87f51158b
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-05-07 03:24:41 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/martini/BoardConfigVendor.mk
